89320c9282f3492aba6a31669b445850aaa5d624
이예준 <yejun@iyejun-ui-MacBookAir.local>
2026-09-20 17:55:30 +0900

    1일차, 섹션 4.회원관리 에제 - 백엔드 개발 / 13.회원 서비스 개발

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello";
-    }
+    } // model에 데이터를 담아서 template에 전달함
 
     @GetMapping("/hello-mvc")
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
@@ -25,8 +25,31 @@ public class HelloController {
     @GetMapping("/hello-string")
     @ResponseBody
     public String helloString(@RequestParam("name") String name) {
-        return "hello" + name;
+        return "hello " + name;
     }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    } // return 에 객체가(hello) 면 default value = json 으로 데이터 전달, 위 예제처럼 문자열 일때는 그냥 문자열 전달
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    } // 그냥 기본적인 자바 특성
+
+
+
 
 
 }
--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -1,4 +1,37 @@
 package hello.hello_spring.service;
 
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
+
+    private final MemberRepository repository = new MemoryMemberRepository();
+
+
+    // 회원가입
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); // 중복회원 검증
+        repository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers() {
+        return repository.findAll();
+    }
+
+    public Optional<Member> findOne (long memId) {
+        return repository.findById(memId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        repository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
 }
